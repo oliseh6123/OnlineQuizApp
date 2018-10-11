f982a75acccd9a18c00a2356e89e579640f82448
Emmanuel Obiajuru <oliseh.obiajuru@gmail.com>
2018-10-11 20:52:38 +0100

    Online Quiz App

---
+++ app/src/main/java/com/sydtech/androidonlinequizapp/Common/Common.java
@@ -0,0 +1,12 @@
+package com.sydtech.androidonlinequizapp.Common;
+
+import com.sydtech.androidonlinequizapp.Model.User;
+
+/**
+ * Created by Emmanuel Obiajuru on 5/22/2018.
+ */
+
+public class Common {
+    public static String categoryId;
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/sydtech/androidonlinequizapp/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.sydtech.androidonlinequizapp.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Emmanuel Obiajuru on 4/5/2018.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view,int position,boolean isLongClick);
+}
